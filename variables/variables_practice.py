5fa6daa0e44845a5838e0eb967a2a05120e3b033
Lorenzo S <73533964+s08302317@users.noreply.github.com>
2021-10-28 12:45:25 -0400

    first update
    extra information

---
+++ variables/variables_practice.py
@@ -0,0 +1,12 @@
+#Variables Practice, Lorenzo Saliard, 10/28/2021 11:40am, version 0.0
+
+#Variable Types-- Most common
+
+my_float = -1.8723
+my_int = -24321334
+my_string = "I like boiled peanuts"
+
+print(my_float)
+print(my_int)
+print(my_string)
+
